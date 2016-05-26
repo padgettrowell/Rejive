018c50f889fb87b54147492cdf9cb6ef586edbfb
padgett.rowell <padgett@padgett.com.au>
2016-05-26 10:30:29 +0800

    irrKlang dll's now embedded as resources, extract on run

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Windows.Forms;
+using Rejive.Services;
 
 namespace Rejive
 {
@@ -9,6 +10,10 @@ namespace Rejive
         [STAThread]
         static void Main()
         {
+            ResourceExtractor.ExtractResourceToFile("Rejive.irrKlang.NET4.dll", "irrKlang.NET4.dll");
+            ResourceExtractor.ExtractResourceToFile("Rejive.ikpMP3.dll", "ikpMP3.dll");
+            ResourceExtractor.ExtractResourceToFile("Rejive.ikpFlac.dll", "ikpFlac.dll");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.ThreadException += Application_ThreadException;
@@ -16,7 +21,6 @@ namespace Rejive
             Session.Profile = ProfileService.LoadProfile();
             Application.Run(new PlayerForm());
        }
-
 
         private static void AppDomain_UnhandledException(object sender, UnhandledExceptionEventArgs args)
         {
